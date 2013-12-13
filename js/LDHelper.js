d8e15f5dd5d1d45b38c82a8aa9f92308d3e3036e
ifkccp <ifkccp@163.com>
2013-12-13 12:18:26 +0800

    rewrite ajax met

--- js/LDHelper.js
+++ js/LDHelper.js
@@ -1,3 +1,23 @@
+var Utils = {}
+
+Utils.get_version = function() {
+	return chrome.app.getDetails().version
+}
+
+Utils.get_by_id = function (id) {
+	return document.getElementById(id)
+}
+
+Utils.get_by_class = function (className) {
+	return document.getElementsByClassName(className)[0]
+}
+
+Utils.get_by_tag = function(obj, tagName) {
+	return obj.getElementsByTagName(tagName)
+}
+
+
+
 var LDHelper = (function(){
 	var _L = function(){
 		this.has_init = false
@@ -49,29 +69,35 @@ var LDHelper = (function(){
 		}
 
 		this.user = {}
-		this.hash = ''
+		this.token = ''
 
 		// 迭代
 		this.it_name = ''
 		this.it_box = []
-		this.it_index = -1
+		this.it_index = 0
+		this.it_total = 0
+
+		this.delay = 0
+		this.tank = []
 	}
 
 	_L.prototype = {
 		"next_action": null,
 		"init": function(settings){
-			if(this.has_init) { return }
+			if(this.has_init) return
 			this.has_init = true
 
+			this.settings = settings
 			var host = window.location.host
 			host = host.substr(0, host.indexOf('.')) // sns & bbs
+			host = 'bbs'
 			this.host = host
 
 			var _box = document.createElement('div')
 			var words = ''
 
 			var s = '<div id="LDH_mask"></div>'
-			s += '<div id="LDH_log"><h3>LDHelper 1.0.0<span id="LDH_close"></span></h3><div>'+words+'<br /><span id="LDH_start"> start </span></div></div>'
+			s += '<div id="LDH_log"><h3>LDHelper 1.0.0<span id="LDH_name"></span><span id="LDH_close"></span></h3><div>'+words+'<br /><span id="LDH_start">开搞！</span></div></div>'
 			_box.innerHTML = s
 			document.body.appendChild(_box)
 
@@ -97,14 +123,15 @@ var LDHelper = (function(){
 			this.log_wrapper.appendChild(_l)
 		},
 		"run": function(){
+			if(this.is_running) return
+			this.is_running = true
+
 			this.log("正在获取当前绿点状态...", "ing")
 			var self = this
-			if('sns' == this.host) {
-				_L.next_action = this.visit
-			} else {
-				_L.next_action = this.cmt
-			}
-			Http.get(this.url_pool['space'], function(r){
+
+			this.next_action = this.get_info
+
+			this.ajax_call = function(r){
 				if(r.indexOf('请先登录后才能继续浏览') !== -1)
 				{
 					alert('木有登录你刷个P啊~~')
@@ -112,22 +139,30 @@ var LDHelper = (function(){
 				}
 				else
 				{
-					var ms = r.match(/discuz_uid\s=\s'(\d+)'/)
-					self.user['uid'] = ms[1]
-					ms = r.match(/pacecp&ac=credit">(\d+)<\/a><li>绿币/)
-					self.ld_start = ms[1]
+					// var ms = r.match(/discuz_uid\s=\s'(\d+)'/)
+					// self.user['uid'] = ms[1]
+					// ms = r.match(/pacecp&ac=credit">(\d+)<\/a><li>绿币/)
+					// self.ld_start = ms[1]
+					self.ld_start = 1
 					self.log("当前拥有"+self.ld_start+"个绿点，土豪，我们做朋友吧~~", "succ")
-
-					_L.next_action()
 				}
-			})
+			}
+
+			this.get(this.url_pool['space'])
+		},
+		"get_info": function(){
+			this.log("获取信息")
+			this.next_action = this.get_token
+
+			this.ajax_call = function(r){
+				Utils.get_by_id('LDH_name').innerHTML = '小井嗨嗨~~'
+			}
+
+			this.get("a")
 		},
 		"visit": function() {
-			console.log(this)
 			if(!this.it_name)
 			{
-				console.log(this)
-				console.log(this.log)
 				this.log("正在访问好友页面...", "ing")
 				var ids = this.settings.frds
 				ids += "\n1163670"
@@ -138,27 +173,318 @@ var LDHelper = (function(){
 				this.it_name = 'frds'
 				this.it_box = ids
 				this.it_index = 0
-				_L.next_action = this.visit
+				this.it_total = ids.length
 			}
 
 			var id = this.it_box[this.it_index]
 			var self = this
-			_L.next_action = this.twit
-			if(this.it_index == this.it_box.length) {
-				_L.next_action = this.twit
+
+			this.ajax_call = function(r){
+				if(self.it_index >= self.it_total) {
+					self.log("成功访问10个好友", "succ")
+					self.next_action = self.gift_get
+					self.it_name = ''
+				}
+				self.it_index++
+			}
+			this.get(this.url_pool['visit'].replace('[UID]', id))
+		},
+		"gift_get": function() {
+			this.log("正在领取红包...", "ing")
+
+			var self = this
+			this.ajax_call = function(r){
+				if(r.indexOf('恭喜您，任务已成功完成') != -1) {
+					self.log("领取成功，正在出售...", "succ")
+					self.next_action = gift_sell
+				} else {
+					self.next_action = this.twit
+				}
 			}
 
-			Http.get(this.url_pool['visit'].replace('[UID]', id), function(r){
-				self.it_index++
-				if(self.it_index == self.it_box.length) {
-					self.log("成功访问10个好友", "succ")
-				}
-				_L.next_action()
-			})
+			this.get(this.url_pool['gift_get'])
 		},
-		"twit": function() { alert('twit') },
-		"cmt": function() { alert('cmt') },
+		"gift_sell": function() {
+			this.next_action = this.twit
+
+			this.ajax_call = function(r){
+				self.log("红包出售成功，获得45绿点", "succ")
+			}
+
+			var post_data = {
+				"formhash": this.token,
+				"handlekey": "magics",
+				"operation": "sell",
+				"magicid": "2",
+				"magicnum": "1",
+				"operatesubmit": "yes",
+			}
+
+			this.post(this.url_pool['gift_sell'], post_data)
+		},
+		"twit": function() {
+			if(!this.it_name)
+			{
+				this.log("正在发表记录...", "ing")
+				var words = this.settings.twit.split("\n")
+
+				this.it_name = 'twit'
+				this.it_box = words
+				this.it_total = 5
+				this.it_index = 0
+
+			}
+
+			var self = this
+			this.ajax_call = function(r){
+				if(self.it_index >= self.it_total) {
+				self.log("成功发表5条记录", "succ")
+
+					self.next_action = this.blog
+					self.it_name = ''
+				}
+				self.it_index++
+			}
+
+			var post_data = {
+				"add": "",
+				"addsubmit": "true",
+				"refer": "home.php?mod=space&uid="+this.user['uid']+"&do=doing&view=me&from=space",
+				"topicid": "",
+				"formhash": this.token
+			}
+
+			this.post(this.url_pool['twit_post'], post_data)
+		},
+		"blog": function() {
+			if(!this.it_name){
+				this.log("正在发表日志...", "ing")
+				var words = this.settings.blog.split("\n")
+
+				this.it_name = 'blog'
+				this.it_box = words
+				this.it_total = 5
+				this.it_index = 0
+				this.delay = 1000
+			}
+
+			var self = this
+			this.ajax_call = function(r){
+				if(self.it_index >= self.it_total) {
+					self.log("成功发表5篇博客", "succ")
+					this.delay = 0
+					self.next_action = this.finish
+				}
+				self.it_index++
+			}
+
+			var post_data = {
+				"subject": "t3",
+				"message": "n3",
+				"retid": "0",
+				"rekeyid": "0",
+				"eventid": "-1",
+				"classid": "0",
+				"tag": "",
+				"friend": "0",
+				"password": "",
+				"selectgroup": "",
+				"target_names": "",
+				"makefeed": "1",
+				"blogsubmit": "true",
+				"formhash": this.token
+			}
+
+			this.post(this.url_pool['blog_post'], post_data)
+		},
+		"finish": function(){
+			this.log("正在获取当前绿点状态...", "ing")
+			var self = this
+
+			this.ajax_call = function(r){
+				// var ms = r.match(/discuz_uid\s=\s'(\d+)'/)
+				// self.user['uid'] = ms[1]
+				// ms = r.match(/pacecp&ac=credit">(\d+)<\/a><li>绿币/)
+				// self.ld_start = ms[1]
+				self.ld_start = 1
+				self.log("当前拥有"+self.ld_start+"个绿点，土豪，我们做朋友吧~~", "succ")
+
+				self.next_action = self.report
+			}
+
+			this.get(this.url_pool['space'])
+		},
+		"report": function(){
+			this.log("上报")
+			this.next_action = null
+
+			this.ajax_call = function(r){
+
+			}
+
+			this.get("a")
+		},
+		"cmt": function() {
+			this.log("正在风版回帖：", "ing")
+
+			var self = this
+			this.next_action = this.cmt_feng
+
+			this.ajax_call = function(r){
+				var ms = r.match(/normalthread_(\d+)/g)
+				self.tank = []
+				for(i in ms) {
+					self.tank.push(ms[i].substr(13))
+					if(self.tank.length >= 40) break
+				}
+			}
+
+			this.get(this.url_pool['thread_feng_main'])
+		},
+		"cmt_feng": function () {
+			if(!this.it_name) {
+				var words = this.settings.cmt.split("\n")
+
+				this.it_name = 'feng_cmt'
+				this.it_box = words
+				this.it_total = 40
+				this.it_index = 0
+				this.delay = 10
+			}
+
+			var self = this
+			this.ajax_call = function(r){
+				if(self.it_index >= self.it_total) {
+					self.it_name = ''
+					self.next_action = self.cmt_n
+					self.delay = 0
+				}
+				self.it_index++
+			}
+
+			var post_data = {
+				'message':'嗯嗯嗯~~~',
+				'posttime':'1',
+				'formhash':this.hash,
+				'subject':''
+			}
+
+			post_data['posttime'] = Date.parse(new Date())
+			post_data['message'] = 'a'
+			var url = this.url_pool['thread_post'].replace('[TID]', this.tank[this.it_index])
+			this.post(url, post_data)
+		},
+		"cmt_n": function(){
+			this.log("正在普通版回帖：", "ing")
+
+			var self = this
+			this.next_action = this.cmt_nomo
+
+			this.ajax_call = function(r){
+				var ms = r.match(/normalthread_(\d+)/g)
+				self.tank = []
+				for(i in ms) {
+					self.tank.push(ms[i].substr(13))
+					if(self.tank.length >= 20) break
+				}
+			}
+
+			this.get(this.url_pool['thread_normal_main'])
+		},
+		"cmt_nomo": function(){
+			if(!this.it_name) {
+				var words = this.settings.cmt.split("\n")
+
+				this.it_name = 'nomo_cmt'
+				this.it_box = words
+				this.it_total = 20
+				this.it_index = 0
+				this.delay = 20
+			}
+
+			var self = this
+			this.ajax_call = function(r){
+				if(self.it_index >= self.it_total) {
+					self.it_name = ''
+					self.next_action = self.finish
+				}
+				self.it_index++
+			}
+
+			var post_data = {
+				'message':'嗯嗯嗯~~~',
+				'posttime':'1',
+				'formhash':this.hash,
+				'subject':''
+			}
+
+			post_data['posttime'] = Date.parse(new Date())
+			post_data['message'] = 'a'
+			var url = this.url_pool['thread_post'].replace('[TID]', this.tank[this.it_index])
+			this.post(url, post_data)
+		},
+		"request": function (type, url, data) {
+			url = "http://www.v5snj.com?url=" + encodeURIComponent(url)
+			var xhr = new XMLHttpRequest()
+			xhr.open(type, url)
+			var self = this
+			xhr.onreadystatechange = function() {
+				if(4 == xhr.readyState && 200 == xhr.status) {
+					self.ajax_call(xhr.responseText)
+					if(self.next_action){
+						if(self.delay) {
+							setTimeout(function(){self.next_action()}, self.delay)
+						} else {
+							self.next_action()
+						}
+					}
+				}
+			}
+
+			if("GET" == type) {
+				xhr.send()
+			}else{
+				var post_data = ''
+				for(i in data) {
+					post_data += i + '=' + encodeURIComponent(data[i]) + '&'
+				}
+				if(post_data) {
+					post_data = post_data.substr(0, post_data.length - 1)
+				}
+
+				xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+				xhr.send(post_data)
+			}
+		},
+		"get": function(url){
+			this.request("GET", url)
+		},
+		"post": function(url, data){
+			this.request("POST", url, data)
+		},
+		"get_token": function(){
+			if('sns' == this.host) {
+				this.next_action = this.visit
+			} else {
+				this.next_action = this.cmt
+			}
+
+			var self = this
+			this.ajax_call = function(r){
+				var ms = r.match(/formhash"\svalue="(.*?)"/)
+				// self.token = ms[1]
+			}
+
+			this.get(this.url_pool['gift_sell_form'])
+		}
 	}
 
 	return new _L
 })()
+
+LDHelper.init({
+	'frds': "1\n2",
+	"twit": "a\nb\nc",
+	"blog": "a###bbb",
+	"cmt": 'a'
+})
